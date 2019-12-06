28a8b97a30cacbf9d25e7376dfb0e5402570a2b8
jlalus99 <jakub.la1511@gmail.com>
2019-12-06 13:39:34 +0100

    Add initial script

---
+++ initial.sh
@@ -0,0 +1,17 @@
+#welcome in initil script!
+#change SSID and PASSWORD to  your names
+#sudo nmcli dev wifi connect "SSID" password "PASSWORD" infname wlan0
+
+sudo apt-get update
+
+sudo python -m pip install --upgrade pip setuptools wheel
+sudo pip install Pillow
+
+sudo apt install python-dev python-pip libfreetype6-dev libjpeg-dev build-essential libopenjp2-7 libtiff5
+sudo apt install libsdl-dev libportmidi-dev libsdl-ttf2.0-dev libsdl-mixer1.2-dev libsdl-image1.2-dev
+git clone https://github.com/rm-hull/luma.examples.git
+cd luma.examples
+sudo -H pip install --upgrade luma.oled
+
+# run example
+sudo python clock.py --i2c-port 0
